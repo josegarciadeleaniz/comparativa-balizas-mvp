7757f61f902a343b6adc341fd195803544864fdb
josegarciadeleaniz <jose@blnz.es>
2026-01-07 12:01:36 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,6 +4,19 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
+// ===============================
+// CORS (PERMITIR balizas.pro)
+// ===============================
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://balizas.pro');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use(express.json());
 
 // ===============================
